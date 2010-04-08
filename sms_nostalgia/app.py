8c59957f2e3743a4da9ead51681e7438b3399f52
Tomasz Nazar <tomasz.nazar@gmail.com>
2010-04-08 04:13:53 +0200

    Portrait mode added - courtesy of gPodder (http://wiki.maemo.org/PyMaemo/Portrait_mode)

--- sms_nostalgia/app.py
+++ sms_nostalgia/app.py
@@ -6,6 +6,7 @@ from model.root import Root
 from view.view import View
 from controller.controller import Controller
 
+from lib.portrait import FremantleRotation
 
 root = Root()
 root.build_data()
@@ -14,6 +15,12 @@ controller = Controller(root)
 view = View(root, controller)
 
 controller.view = view
+
+
+rotation_object = FremantleRotation('SmsNostalgia', 
+                                    main_window=view.window_main.window, 
+                                    version='1.0', 
+                                    mode=FremantleRotation.AUTOMATIC)
 
 
 view.start()
